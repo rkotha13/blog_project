860d20d0900d39e8d9e2a2e773832d7036d682fb
rkotha13 <rkotha13@asu.edu>
2025-07-05 12:13:55 -0700

    login functionality

--- myblogsite/urls.py
+++ myblogsite/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logged_out.html'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
--- myblogsite/views.py
+++ myblogsite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import BlogPost
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
